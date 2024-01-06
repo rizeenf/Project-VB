ca4ec4648acfea030f293667b57f8ba0160851fc
rizeenf <rize.nf11@gmail.com>
2024-01-06 11:48:50 +0700

    Add: Data grid History books

--- Form2.Designer.vb
+++ Form2.Designer.vb
@@ -1,9 +1,9 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
 Partial Class Form2
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
-    <System.Diagnostics.DebuggerNonUserCode()> _
+    <System.Diagnostics.DebuggerNonUserCode()>
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
         Try
             If disposing AndAlso components IsNot Nothing Then
@@ -20,7 +20,7 @@ Partial Class Form2
     'NOTE: The following procedure is required by the Windows Form Designer
     'It can be modified using the Windows Form Designer.  
     'Do not modify it using the code editor.
-    <System.Diagnostics.DebuggerStepThrough()> _
+    <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
         Me.Label1 = New System.Windows.Forms.Label()
         Me.Label2 = New System.Windows.Forms.Label()
@@ -35,138 +35,226 @@ Partial Class Form2
         Me.TextBox3 = New System.Windows.Forms.TextBox()
         Me.TextBox4 = New System.Windows.Forms.TextBox()
         Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.GroupBox3 = New System.Windows.Forms.GroupBox()
+        Me.DataGridView2 = New System.Windows.Forms.DataGridView()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
         CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        Me.GroupBox3.SuspendLayout()
+        CType(Me.DataGridView2, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(50, 28)
+        Me.Label1.Location = New System.Drawing.Point(11, 34)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(39, 13)
+        Me.Label1.Size = New System.Drawing.Size(59, 13)
         Me.Label1.TabIndex = 0
-        Me.Label1.Text = "Label1"
+        Me.Label1.Text = "Kode buku"
         '
         'Label2
         '
         Me.Label2.AutoSize = True
-        Me.Label2.Location = New System.Drawing.Point(52, 67)
+        Me.Label2.Location = New System.Drawing.Point(11, 60)
         Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(39, 13)
+        Me.Label2.Size = New System.Drawing.Size(62, 13)
         Me.Label2.TabIndex = 1
-        Me.Label2.Text = "Label2"
+        Me.Label2.Text = "Nama buku"
         '
         'Label3
         '
         Me.Label3.AutoSize = True
-        Me.Label3.Location = New System.Drawing.Point(56, 117)
+        Me.Label3.Location = New System.Drawing.Point(11, 86)
         Me.Label3.Name = "Label3"
-        Me.Label3.Size = New System.Drawing.Size(39, 13)
+        Me.Label3.Size = New System.Drawing.Size(62, 13)
         Me.Label3.TabIndex = 2
-        Me.Label3.Text = "Label3"
+        Me.Label3.Text = "Stock buku"
         '
         'Label4
         '
         Me.Label4.AutoSize = True
-        Me.Label4.Location = New System.Drawing.Point(413, 11)
+        Me.Label4.Location = New System.Drawing.Point(10, 24)
         Me.Label4.Name = "Label4"
-        Me.Label4.Size = New System.Drawing.Size(39, 13)
+        Me.Label4.Size = New System.Drawing.Size(25, 13)
         Me.Label4.TabIndex = 3
-        Me.Label4.Text = "Label4"
+        Me.Label4.Text = "Cari"
         '
         'Button1
         '
-        Me.Button1.Location = New System.Drawing.Point(59, 150)
+        Me.Button1.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.Location = New System.Drawing.Point(387, 60)
         Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.Size = New System.Drawing.Size(100, 46)
         Me.Button1.TabIndex = 4
-        Me.Button1.Text = "Button1"
+        Me.Button1.Text = "Tambah Buku"
         Me.Button1.UseVisualStyleBackColor = True
         '
         'Button2
         '
-        Me.Button2.Location = New System.Drawing.Point(269, 152)
+        Me.Button2.Location = New System.Drawing.Point(152, 19)
         Me.Button2.Name = "Button2"
         Me.Button2.Size = New System.Drawing.Size(75, 23)
         Me.Button2.TabIndex = 5
-        Me.Button2.Text = "Button2"
+        Me.Button2.Text = "Update"
         Me.Button2.UseVisualStyleBackColor = True
         '
         'Button3
         '
-        Me.Button3.Location = New System.Drawing.Point(441, 154)
+        Me.Button3.Location = New System.Drawing.Point(233, 19)
         Me.Button3.Name = "Button3"
         Me.Button3.Size = New System.Drawing.Size(75, 23)
         Me.Button3.TabIndex = 6
-        Me.Button3.Text = "Button3"
+        Me.Button3.Text = "Hapus"
         Me.Button3.UseVisualStyleBackColor = True
         '
         'Button4
         '
-        Me.Button4.Location = New System.Drawing.Point(551, 179)
+        Me.Button4.Location = New System.Drawing.Point(386, 19)
         Me.Button4.Name = "Button4"
-        Me.Button4.Size = New System.Drawing.Size(100, 110)
+        Me.Button4.Size = New System.Drawing.Size(100, 23)
         Me.Button4.TabIndex = 7
-        Me.Button4.Text = "Button4"
+        Me.Button4.Text = "Refresh"
         Me.Button4.UseVisualStyleBackColor = True
         '
         'TextBox1
         '
-        Me.TextBox1.Location = New System.Drawing.Point(141, 30)
+        Me.TextBox1.Location = New System.Drawing.Point(117, 31)
         Me.TextBox1.Name = "TextBox1"
-        Me.TextBox1.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox1.Size = New System.Drawing.Size(55, 20)
         Me.TextBox1.TabIndex = 8
         '
         'TextBox2
         '
-        Me.TextBox2.Location = New System.Drawing.Point(141, 64)
+        Me.TextBox2.Location = New System.Drawing.Point(117, 57)
         Me.TextBox2.Name = "TextBox2"
-        Me.TextBox2.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox2.Size = New System.Drawing.Size(200, 20)
         Me.TextBox2.TabIndex = 9
         '
         'TextBox3
         '
-        Me.TextBox3.Location = New System.Drawing.Point(141, 110)
+        Me.TextBox3.Location = New System.Drawing.Point(117, 83)
         Me.TextBox3.Name = "TextBox3"
-        Me.TextBox3.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox3.Size = New System.Drawing.Size(200, 20)
         Me.TextBox3.TabIndex = 10
         '
         'TextBox4
         '
-        Me.TextBox4.Location = New System.Drawing.Point(416, 30)
+        Me.TextBox4.Location = New System.Drawing.Point(41, 21)
         Me.TextBox4.Name = "TextBox4"
-        Me.TextBox4.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox4.Size = New System.Drawing.Size(105, 20)
         Me.TextBox4.TabIndex = 11
         '
         'DataGridView1
         '
         Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
-        Me.DataGridView1.Location = New System.Drawing.Point(38, 181)
+        Me.DataGridView1.Location = New System.Drawing.Point(13, 50)
         Me.DataGridView1.Name = "DataGridView1"
-        Me.DataGridView1.Size = New System.Drawing.Size(494, 119)
+        Me.DataGridView1.Size = New System.Drawing.Size(473, 100)
         Me.DataGridView1.TabIndex = 12
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.TextBox3)
+        Me.GroupBox1.Controls.Add(Me.TextBox2)
+        Me.GroupBox1.Controls.Add(Me.TextBox1)
+        Me.GroupBox1.Controls.Add(Me.Button1)
+        Me.GroupBox1.Controls.Add(Me.Label3)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Location = New System.Drawing.Point(5, 78)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(501, 111)
+        Me.GroupBox1.TabIndex = 13
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Detail buku"
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.DataGridView1)
+        Me.GroupBox2.Controls.Add(Me.TextBox4)
+        Me.GroupBox2.Controls.Add(Me.Button4)
+        Me.GroupBox2.Controls.Add(Me.Button3)
+        Me.GroupBox2.Controls.Add(Me.Button2)
+        Me.GroupBox2.Controls.Add(Me.Label4)
+        Me.GroupBox2.Location = New System.Drawing.Point(6, 197)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(500, 160)
+        Me.GroupBox2.TabIndex = 14
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "List buku"
+        '
+        'GroupBox3
+        '
+        Me.GroupBox3.Controls.Add(Me.DataGridView2)
+        Me.GroupBox3.Location = New System.Drawing.Point(522, 78)
+        Me.GroupBox3.Name = "GroupBox3"
+        Me.GroupBox3.Size = New System.Drawing.Size(498, 278)
+        Me.GroupBox3.TabIndex = 15
+        Me.GroupBox3.TabStop = False
+        Me.GroupBox3.Text = "History buku"
+        '
+        'DataGridView2
+        '
+        Me.DataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView2.Location = New System.Drawing.Point(6, 19)
+        Me.DataGridView2.Name = "DataGridView2"
+        Me.DataGridView2.Size = New System.Drawing.Size(486, 250)
+        Me.DataGridView2.TabIndex = 0
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(577, 62)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(31, 13)
+        Me.Label5.TabIndex = 16
+        Me.Label5.Text = "2023"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(519, 62)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(52, 13)
+        Me.Label6.TabIndex = 17
+        Me.Label6.Text = "Tanggal :"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label7.Location = New System.Drawing.Point(388, 21)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(296, 20)
+        Me.Label7.TabIndex = 18
+        Me.Label7.Text = "DATA PERPUSTAKAAN NASIONAL"
         '
         'Form2
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(687, 308)
-        Me.Controls.Add(Me.DataGridView1)
-        Me.Controls.Add(Me.TextBox4)
-        Me.Controls.Add(Me.TextBox3)
-        Me.Controls.Add(Me.TextBox2)
-        Me.Controls.Add(Me.TextBox1)
-        Me.Controls.Add(Me.Button4)
-        Me.Controls.Add(Me.Button3)
-        Me.Controls.Add(Me.Button2)
-        Me.Controls.Add(Me.Button1)
-        Me.Controls.Add(Me.Label4)
-        Me.Controls.Add(Me.Label3)
-        Me.Controls.Add(Me.Label2)
-        Me.Controls.Add(Me.Label1)
+        Me.ClientSize = New System.Drawing.Size(1032, 367)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.GroupBox3)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.GroupBox1)
         Me.Name = "Form2"
         Me.Text = "Form2"
         CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
+        Me.GroupBox3.ResumeLayout(False)
+        CType(Me.DataGridView2, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
@@ -185,4 +273,11 @@ Partial Class Form2
     Friend WithEvents TextBox3 As TextBox
     Friend WithEvents TextBox4 As TextBox
     Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents GroupBox3 As GroupBox
+    Friend WithEvents DataGridView2 As DataGridView
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label7 As Label
 End Class
--- Form2.vb
+++ Form2.vb
@@ -3,22 +3,34 @@
 Public Class Form2
     'buat variabel tanpa type data
     Dim kodesd
+    Dim DateNow As String = DateTime.Now.ToString("yyyy/MM/dd HH:mm:ss")
+
     Private Sub Form2_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         koneksi()
-        Label1.Text = "kode barang"
-        Label2.Text = "nama Barang"
-        Label3.Text = "stock barang"
-        Label4.Text = "pencarian Data (nama)"
-        Button1.Text = "Simpan Data"
-        Button2.Text = "Update/ Edit Data"
-        Button3.Text = "Hapus Data"
-        Button4.Text = "Refresh Data"
-        ' supaya kode ga bisa diotak-atik
+        Label5.Text = DateNow
+        DataGridView1.AllowUserToAddRows = False
+        DataGridView2.AllowUserToAddRows = False
         TextBox1.ReadOnly = True
-        ' buat prosedur membersihkan objek
-        bersih()
+        Bersih()
+        Resize_Grid()
     End Sub
-    Sub bersih()
+
+    Sub Resize_Grid()
+        With DataGridView1
+            .Columns(0).Width = 30
+            .Columns(1).Width = 200
+            .Columns(2).Width = 100
+        End With
+
+        With DataGridView2
+            .Columns(0).Width = 30
+            .Columns(1).Width = 200
+            .Columns(2).Width = 70
+            .Columns(3).Width = 140
+        End With
+
+    End Sub
+    Sub Bersih()
         TextBox1.Text = ""
         TextBox2.Text = ""
         TextBox3.Text = ""
@@ -27,19 +39,29 @@ Public Class Form2
         Me.Button1.Enabled = True
         Me.Button2.Enabled = False
         Me.Button3.Enabled = False
-        tampil_data()
+        Tampil_data_buku()
+        Tampil_data_history()
     End Sub
-    Sub tampil_data()
+    Sub Tampil_data_buku()
         '------- Untuk menampilkan data di datagrid -----------
-        da = New MySqlDataAdapter("select * from product order by productId", con)
+        da = New MySqlDataAdapter("select productId as No, product_name as Nama_Buku, product_qty as Stok, product_price as Tahun from product order by productId", con)
         ds = New DataSet
         ds.Clear()
         da.Fill(ds, "product")
         Me.DataGridView1.DataSource = (ds.Tables("product"))
-        '-------------- diatas codingnya ---------------
-        nomor()
+        '-------------- Diatas codingnya ---------------
+        Nomor()
     End Sub
-    Sub nomor()
+    Sub Tampil_data_history()
+        '------- Untuk menampilkan data di datagrid -----------
+        da = New MySqlDataAdapter("select historyId as NO, history_name as Nama, history_qty as Updated_Qty, history_date as Tanggal_Update from history order by historyId", con)
+        ds = New DataSet
+        ds.Clear()
+        da.Fill(ds, "history")
+        Me.DataGridView2.DataSource = (ds.Tables("history"))
+        '-------------- Diatas codingnya ---------------
+    End Sub
+    Sub Nomor()
         Dim DR As DataRow
         Dim s As String
         'mengambil kode dari field ID, kemudian dicari nilai yg paling besar (max)
@@ -47,31 +69,34 @@ Public Class Form2
         DR = SQLTable("select max(right(productId,1)) as nomor from product").Rows(0)
         'jika berisi null atau tdk ditemukan
         If DR.IsNull("Nomor") Then
-            s = "KS-1" 'member nilai awal
+            s = "Book-1" 'member nilai awal
         Else
-            s = "KS-" & Format(DR("Nomor") + 1, "0")
+            s = "Book-" & Format(DR("Nomor") + 1, "0")
         End If
         TextBox1.Text = s
         TextBox1.Enabled = False
     End Sub
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
         '------- Coding Simpan --------
-        If Me.TextBox1.Text = "" Or Me.TextBox2.Text = "" Or Me.TextBox3.Text =
-        "" Then
-            MsgBox(" Maaf datanya belum lengkap mas bro")
+        If Me.TextBox1.Text = "" Or Me.TextBox2.Text = "" Or Me.TextBox3.Text = "" Then
+            MsgBox("Maaf datanya belum lengkap mas bro")
         Else
             Dim simpan As String
-            MsgBox("data anda akan disimpan mas bro")
+            Dim tambahHistory As String
+
+            MsgBox("Data berhasil ditambahkan")
             simpan = "insert into product (productId,product_name,product_qty)values('" & Me.TextBox1.Text & "','" & Me.TextBox2.Text & "','" & Me.TextBox3.Text & "')"
+            tambahHistory = "insert into history (historyId,history_name,history_qty, history_date)values('" & Me.TextBox1.Text & "','" & Me.TextBox2.Text & "','" & Me.TextBox3.Text & "', '" & DateNow & "')"
             cmd = New MySqlCommand(simpan, con) ' untuk menghubungkan ke database dan table lalu simpan
+            cmd = New MySqlCommand(tambahHistory, con) ' untuk menghubungkan ke database dan table lalu simpan
 
             cmd.ExecuteNonQuery() ' mengeksekusi datanya
-            bersih()
+            Bersih()
         End If
     End Sub
     Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
         If Me.TextBox1.Text = "" Or TextBox2.Text = "" Then
-            MsgBox(" Maaf datanya tidak ada yang di update")
+            MsgBox("Maaf datanya tidak ada yang di update")
         Else
             ' ------- Coding update data --------
             MsgBox("Anda akan mengupdate datanya ya")
@@ -80,13 +105,13 @@ Public Class Form2
             cmd = New MySqlCommand(edit, con) ' untuk menghubungkan ke database dan table lalu Update
 
             cmd.ExecuteNonQuery() ' mengeksekusi datanya
-            bersih()
+            Bersih()
         End If
     End Sub
     Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
         If Me.TextBox1.Text = "" Or Me.TextBox2.Text = "" Then
             MsgBox(" Maaf datanya tidak ada yang di hapus")
-            bersih()
+            Bersih()
         Else
             ' ------- Coding update data --------
             If MessageBox.Show("Anda yakin akan menghapus datanya ?", "", MessageBoxButtons.YesNo) = Windows.Forms.DialogResult.Yes Then
@@ -94,20 +119,23 @@ Public Class Form2
                 hapus = "delete from product where productId = '" & Me.TextBox1.Text & "'"
                 cmd = New MySqlCommand(hapus, con)
                 cmd.ExecuteNonQuery()
-                bersih()
+                Bersih()
             Else
             End If
         End If
     End Sub
     Private Sub DataGridView1_CellClick(sender As Object, e As DataGridViewCellEventArgs) Handles DataGridView1.CellClick
         ' fungsi untuk menclik data yang akan dipilih
-        kodesd = DataGridView1.Rows(e.RowIndex).Cells(0).Value
-        TextBox1.Text = kodesd
-        TextBox2.Text = DataGridView1.Rows(e.RowIndex).Cells(1).Value
-        TextBox3.Text = DataGridView1.Rows(e.RowIndex).Cells(2).Value
-        Me.Button1.Enabled = False
-        Me.Button2.Enabled = True
-        Me.Button3.Enabled = True
+        If e.RowIndex >= 0 AndAlso e.RowIndex < DataGridView1.Rows.Count AndAlso e.ColumnIndex >= 0 AndAlso e.ColumnIndex < DataGridView1.Columns.Count Then
+            kodesd = DataGridView1.Rows(e.RowIndex).Cells(0).Value
+            TextBox1.Text = kodesd
+            TextBox2.Text = DataGridView1.Rows(e.RowIndex).Cells(1).Value
+            TextBox3.Text = DataGridView1.Rows(e.RowIndex).Cells(2).Value
+            Me.Button1.Enabled = False
+            Me.Button2.Enabled = True
+            Me.Button3.Enabled = True
+        End If
+
     End Sub
     Private Sub TextBox4_TextChanged(sender As Object, e As EventArgs) Handles TextBox4.TextChanged
         '----------- menyaring di datagrid dengan nama barang---------
@@ -118,6 +146,7 @@ Public Class Form2
         DataGridView1.DataSource = (ds.Tables("product"))
     End Sub
     Private Sub button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
-        bersih()
+        Bersih()
     End Sub
+
 End Class
